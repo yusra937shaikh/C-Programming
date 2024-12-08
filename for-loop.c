52035054e67c1d0ff472ed92fbbd406d82afbeb5
yusra937shaikh <yusra937shaikh@gmail.com>
2024-12-08 15:11:48 +0000

    Update for-loop.c

--- for-loop.c
+++ for-loop.c
@@ -17,3 +17,17 @@ int main()
   }
 return 0;
 }
+
+
+#include<stdio.h>
+int main()
+{
+  int n,i,f=1;
+  printf("\n ENTER THE NUMBER:");
+  scanf("%d", &n);
+  for(i=n;i>=1;i--)
+    {
+      f*=i;
+    }
+printf("\n FACTORIAL OF %d is :%d",n,f);
+}
